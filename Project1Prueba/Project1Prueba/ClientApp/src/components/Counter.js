f177de66f0f7f20164e8a28820747220c8013364
CarlosAlvarado-Dig <carlos.alvarado@digifact.com.gt>
2023-08-16 08:22:37 -0600

    Update Counter.js
    prueba de load design

--- Project1Prueba/Project1Prueba/ClientApp/src/components/Counter.js
+++ Project1Prueba/Project1Prueba/ClientApp/src/components/Counter.js
@@ -3,49 +3,32 @@ import './style.css';
 import sample from './sample.json';
 import EmailEditor from 'react-email-editor';
 
-const designJSON = { "counters": { "u_column": 1, "u_row": 1, "u_content_image": 1 }, "body": { "rows": [{ "cells": [1], "columns": [{ "contents": [{ "type": "image", "values": { "containerPadding": "10px", "_meta": { "htmlID": "u_content_image_1", "htmlClassNames": "u_content_image" }, "selectable": true, "draggable": true, "duplicatable": true, "deletable": true, "src": { "url": "{{img_logo}}" }, "textAlign": "center", "altText": "Image", "action": { "name": "web", "values": { "href": "", "target": "_blank" } }, "hideDesktop": false, "hideMobile": false } }], "values": { "backgroundColor": "", "padding": "0px", "border": {}, "_meta": { "htmlID": "u_column_1", "htmlClassNames": "u_column" } } }], "values": { "displayCondition": null, "columns": false, "backgroundColor": "", "columnsBackgroundColor": "", "backgroundImage": { "url": "", "fullWidth": true, "repeat": false, "center": true, "cover": false }, "padding": "0px", "hideDesktop": false, "hideMobile": false, "noStackMobile": false, "_meta": { "htmlID": "u_row_1", "htmlClassNames": "u_row" }, "selectable": true, "draggable": true, "duplicatable": true, "deletable": true } }], "values": { "backgroundColor": "#e7e7e7", "backgroundImage": { "url": "", "fullWidth": true, "repeat": false, "center": true, "cover": false }, "contentWidth": "500px", "fontFamily": { "label": "Arial", "value": "arial,helvetica,sans-serif" }, "linkStyle": { "body": true, "linkColor": "#0000ee", "linkHoverColor": "#0000ee", "linkUnderline": true, "linkHoverUnderline": true }, "_meta": { "htmlID": "u_body", "htmlClassNames": "u_body" } } }, "schemaVersion": 5 }
 
 export function Counter() {
     const emailEditorRef = useRef(null);
     const [isEditorLoading, setIsEditorLoading] = useState(true);
 
     const exportHtml = () => {
-            //const { design, html } = data;
-            ////console.log('exportHtml', html);
-            //var imageUrl = data.url;
-            //console.log('exportImage', imageUrl);
+        emailEditorRef.current.editor.exportHtml((data) => {
+            const { design, html } = data;
+            console.log('exportHtml', html);
             // save para el "usuario", donde el le de para guardar su diseno
             // aqui mandar el html/json para almacenarlo en la nube/base de datos/ data.
-
-            const options = {
-                method: 'POST',
-                headers: { accept: 'application/json', 'content-type': 'application/json' },
-                body: JSON.stringify({
-                    displayMode: 'email',
-                    design: {},
-                    mergeTags: { first_name: 'John', last_name: 'Doe' },
-                    fullPage: true
-                })
-            };
-
-            fetch('https://api.unlayer.com/v2/export/image', options)
-                .then(response => response.json())
-                .then(response => console.log(response))
-                .catch(err => console.error(err));
+        });
     };
 
     const onLoad = () => {
         // editor instance is created
         // you can load your template here;
         // const templateJson = {};
-        //  emailEditorRef.current.editor.addEventListener(
-        //   'design:loaded',
-        //   onDesignLoad
-        // );
+         // emailEditorRef.current.editor.addEventListener(
+         //  'design:loaded',
+         //  onDesignLoad
+         //);
 
         // conectarnos a la base de datos y en base al ID del diseno, traer el json/html y cargarlos.
         // si viene vacios, dejarlo en blanco. OOOOO uno con fondo blanco.
-        emailEditorRef.current.editor.loadDesign(sample);
+        //emailEditorRef.current.editor.loadDesign(sample);
         emailEditorRef.current.setMergeTags({
             prueba: {
                 name: "PRUEBA",
@@ -59,15 +42,37 @@ export function Counter() {
         // editor is ready
         console.log('onReady');
     };
-
+    const loadTemplate = (id) => {
+        if (id == 1) {
+            emailEditorRef.current.editor.loadDesign(sample);
+        }
+    };
 
 
     return (
-        
-        <html>
+        <div>
             <div>
                 <button onClick={exportHtml}>Export Image</button>
             </div>
+            <div>
+                <button onClick={() => {
+                    loadTemplate(1);
+                }}>Load template 1</button>
+            </div>
+            
+            {/*<div>*/}
+            {/*    <a class="template" onClick={loadTemplate(1)}>*/}
+            {/*        <p>Template 1</p>*/}
+            {/*    </a>*/}
+
+            {/*    <a class="template" onClick={loadTemplate(2)}>*/}
+            {/*        <p>Template 2</p>*/}
+            {/*    </a>*/}
+
+            {/*    <a class="template" onClick={loadTemplate(3)}>*/}
+            {/*        <p>Template 3</p>*/}
+            {/*    </a>*/}
+            {/*</div>*/}
             <br></br>
             <EmailEditor
                 ref={emailEditorRef}
@@ -93,6 +98,6 @@ export function Counter() {
                     sendTestEmail: true
                 }}
             />
-        </html>
+        </div>
     );
 }
